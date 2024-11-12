dcec59cd1939cc8ecb53368e4956fb6be72a47ec
Guiioff <guilhermerivaldy288@hotmail.com>
2024-11-12 17:10:35 -0300

    first commit

---
+++ backend/gestao-consultas-exames/src/main/java/br/com/upe/gestaoconsultasexames/GestaoConsultasExamesApplication.java
@@ -0,0 +1,13 @@
+package br.com.upe.gestaoconsultasexames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoConsultasExamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoConsultasExamesApplication.class, args);
+    }
+
+}
---
+++ backend/gestao-consultas-exames/src/test/java/br/com/upe/gestaoconsultasexames/GestaoConsultasExamesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.upe.gestaoconsultasexames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoConsultasExamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
