a9a391e5d9f02aab7457efa36c1d938b71f1e8e0
Guiioff <guilhermerivaldy288@hotmail.com>
2024-11-13 23:46:08 -0300

    ajustando atributos para predição de ataque cardíaco

--- backend/gestao-consultas-exames/src/main/java/br/com/upe/gestaoconsultasexames/model/ExamePredicaoAtaqueCardiaco.java
+++ backend/gestao-consultas-exames/src/main/java/br/com/upe/gestaoconsultasexames/model/ExamePredicaoAtaqueCardiaco.java
@@ -13,32 +13,68 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Entity
-@Table(name = "exames_predicao_ataque_cardiaco" )
+@Table(name = "exames_predicao_ataque_cardiaco")
 @DiscriminatorValue("PredicaoAtaqueCardiaco")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ExamePredicaoAtaqueCardiaco extends Exame{
+public class ExamePredicaoAtaqueCardiaco extends Exame {
 
-    @Column(name = "cp")
-    private String cp;  // Chest Pain type chest pain type
+    @Column(name = "idade")
+    private Integer idade;
 
-    @Column(name = "trtbps")
-    private Integer trtbps;  // Resting blood pressure (em mmHg)
+    @Column(name = "sexo")
+    private String sexo;
 
-    @Column(name = "chol")
-    private Double chol;  // Cholesterol in mg/dl fetched via BMI sensor
+    @Column(name = "tipo_dor_toracica")
+    private Integer tipoDorToracica;
 
-    @Column(name = "fbs")
-    private Integer fbs;  // Fasting blood sugar > 120 mg/dl (1 = true; 0 = false)
+    @Column(name = "pressao_sanguinea_repouso")
+    private Integer pressaoSanguineaRepouso;
+
+    @Column(name = "colesterol_serico")
+    private Integer colesterolSerico;
+
+    @Column(name = "glicemia_jejum_maior_que_120")
+    private Boolean glicemiaJejumMaiorQue120;
+
+    @Column(name = "resultados_eletrocardiograma_repouso")
+    private Integer resultadosEletrocardiogramaRepouso;
+
+    @Column(name = "frequencia_cardiaca_maxima")
+    private Integer frequenciaCardiacaMaxima;
+
+    @Column(name = "angina_induzida")
+    private Boolean anginaInduzida;
+
+    @Column(name = "depressao_st_induzida")
+    private Float depressaoSTInduzida;
+
+    @Column(name = "inclinacao_pico_st")
+    private Integer inclinacaoPicoST;
+
+    @Column(name = "numero_vasos_principais")
+    private Integer numeroVasosPrincipais;
+
+    @Column(name = "avaliacao_teste_esforco")
+    private String avaliacaoTesteEsforco;
 
     @Override
     public Map<String, Object> coletarDadosExame() {
         Map<String, Object> dados = new HashMap<>();
-        dados.put("cp", cp);
-        dados.put("trtbps", trtbps);
-        dados.put("chol", chol);
-        dados.put("fbs", fbs);
+        dados.put("idade", idade);
+        dados.put("sexo", sexo);
+        dados.put("tipoDorToracica", tipoDorToracica);
+        dados.put("pressaoSanguineaRepouso", pressaoSanguineaRepouso);
+        dados.put("colesterolSerico", colesterolSerico);
+        dados.put("glicemiaJejumMaiorQue120", glicemiaJejumMaiorQue120);
+        dados.put("resultadosEletrocardiogramaRepouso", resultadosEletrocardiogramaRepouso);
+        dados.put("frequenciaCardiacaMaxima", frequenciaCardiacaMaxima);
+        dados.put("anginaInduzida", anginaInduzida);
+        dados.put("depressaoSTInduzida", depressaoSTInduzida);
+        dados.put("inclinacaoPicoST", inclinacaoPicoST);
+        dados.put("numeroVasosPrincipais", numeroVasosPrincipais);
+        dados.put("avaliacaoTesteEsforco", avaliacaoTesteEsforco);
         return dados;
     }
 }
