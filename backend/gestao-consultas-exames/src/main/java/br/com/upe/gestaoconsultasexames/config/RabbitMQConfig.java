caa2a55e2c1a73d1cd2c10f56d3049e8d3485936
Guiioff <guilhermerivaldy288@hotmail.com>
2024-11-16 19:03:01 -0300

    criando classe de configuração para o rabbitmq

---
+++ backend/gestao-consultas-exames/src/main/java/br/com/upe/gestaoconsultasexames/config/RabbitMQConfig.java
@@ -0,0 +1,22 @@
+package br.com.upe.gestaoconsultasexames.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String CONSULTA_QUEUE = "consulta-queue";
+
+    @Bean
+    public Queue consultaQueue() {
+        return new Queue(CONSULTA_QUEUE, true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
