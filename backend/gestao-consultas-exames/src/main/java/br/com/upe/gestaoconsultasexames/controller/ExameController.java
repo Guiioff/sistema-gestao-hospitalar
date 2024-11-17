f37fd469b4eec0c40b70fabfd80dc937bee68925
Guiioff <guilhermerivaldy288@hotmail.com>
2024-11-16 22:57:51 -0300

    criação do controller de exame

---
+++ backend/gestao-consultas-exames/src/main/java/br/com/upe/gestaoconsultasexames/controller/ExameController.java
@@ -0,0 +1,40 @@
+package br.com.upe.gestaoconsultasexames.controller;
+
+import br.com.upe.gestaoconsultasexames.controller.dto.ExamePredicaoAtaqueCardiacoRequest;
+import br.com.upe.gestaoconsultasexames.controller.dto.ExamePredicaoDiabetesRequest;
+import br.com.upe.gestaoconsultasexames.model.Exame;
+import br.com.upe.gestaoconsultasexames.model.enums.TipoExameEnum;
+import br.com.upe.gestaoconsultasexames.service.iExameService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/exames")
+@RequiredArgsConstructor
+public class ExameController {
+
+    private final iExameService exameService;
+
+    @PostMapping("/predicao-ataque-cardiaco")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void realizarExameAtaqueCardiaco(
+            @PathVariable Long exameId,
+            @RequestBody ExamePredicaoAtaqueCardiacoRequest exameRequest) {
+
+        Map<String, Object> dados = exameRequest.toMap();
+        exameService.realizarExame(exameId, dados, TipoExameEnum.valueOf("EXAME_ATAQUE_CARDIACO"));
+    }
+
+    @PostMapping("/predicao-diabetes")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void realizarExameDiabetes(
+            @PathVariable Long exameId,
+            @RequestBody ExamePredicaoDiabetesRequest exameRequest) {
+
+        Map<String, Object> dados = exameRequest.toMap();
+        exameService.realizarExame(exameId, dados, TipoExameEnum.valueOf("EXAME_ATAQUE_CARDIACO"));
+    }
+}
