14b8d12af59ad6bd3855d3b1374f273871a60995
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-19 19:33:53 -0300

    Criando a interface de repositório para a entidade Usuário

---
+++ backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.dev.gabriel.autenticacao.repository;
+
+import com.dev.gabriel.autenticacao.model.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {}
