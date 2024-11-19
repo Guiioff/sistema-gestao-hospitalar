9424b1c9b1ddf231cc47872c6507a919746952db
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-19 20:10:20 -0300

    Implementando handler para as exceções customizadas do sistema

--- backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/exeption/CustomExceptionHandler.java
+++ backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/exeption/CustomExceptionHandler.java
@@ -1,6 +1,26 @@
 package com.dev.gabriel.autenticacao.exeption;
 
+import com.dev.gabriel.autenticacao.dto.response.ErroResponse;
+import com.dev.gabriel.autenticacao.exception.exceptions.GenericaException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.Instant;
+
 @RestControllerAdvice
-public class CustomExceptionHandler {}
+public class CustomExceptionHandler {
+
+  @ExceptionHandler
+  public ResponseEntity<ErroResponse> handleGenericaException(GenericaException ex) {
+    ErroResponse response =
+        ErroResponse.builder()
+            .status(ex.getStatus().value())
+            .erro(ex.getErro())
+            .detalhe(ex.getDetalhe())
+            .timestamp(Instant.now())
+            .build();
+
+    return ResponseEntity.status(ex.getStatus()).body(response);
+  }
+}
