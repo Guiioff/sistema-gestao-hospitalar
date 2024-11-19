eb5972da7c1e403d6187cc1b1daa07b02d6a1bc2
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-19 20:31:16 -0300

    Implementando classe de configuração para a segurança do sistema

---
+++ backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.dev.gabriel.autenticacao.config;
+
+import com.dev.gabriel.autenticacao.utils.KeyUtils;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+
+@Configuration
+public class SecurityConfig {
+  @Value("${security.caminho-chave-privada}")
+  private String caminhoChavePrivada;
+
+  @Value("${security.caminho-chave-publica}")
+  private String caminhoChavePublica;
+
+  private RSAPrivateKey privateKey;
+  private RSAPublicKey publicKey;
+
+  @PostConstruct
+  public void loadKeys() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+    this.privateKey = (RSAPrivateKey) KeyUtils.decodeKey(this.caminhoChavePrivada);
+    this.publicKey = (RSAPublicKey) KeyUtils.decodeKey(this.caminhoChavePublica);
+  }
+}
