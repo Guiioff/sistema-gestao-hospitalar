0e8fee9298a68153f81979168629b7bbfbe642a2
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-19 19:51:54 -0300

    Implementando exceção genérica customizada

---
+++ backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/exception/exceptions/GenericaException.java
@@ -0,0 +1,25 @@
+package com.dev.gabriel.autenticacao.exception.exceptions;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class GenericaException extends RuntimeException {
+  private final HttpStatus status;
+  private final String erro;
+  private final String detalhe;
+
+  public GenericaException(HttpStatus status, String erro, String detalhe) {
+    super(detalhe);
+    this.status = status;
+    this.erro = erro;
+    this.detalhe = detalhe;
+  }
+
+  public GenericaException(String detalhe) {
+    super(detalhe);
+    this.status = HttpStatus.INTERNAL_SERVER_ERROR;
+    this.erro = "Erro interno no sistema";
+    this.detalhe = detalhe;
+  }
+}
