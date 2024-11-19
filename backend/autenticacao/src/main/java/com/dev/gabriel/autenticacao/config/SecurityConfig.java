edcfedd1308e9019130a74cf03cdcd4f89c0c656
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-19 20:36:26 -0300

    Implementando Bean para o encoder dos tokens de autenticação

--- backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/config/SecurityConfig.java
+++ backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/config/SecurityConfig.java
@@ -1,9 +1,17 @@
 package com.dev.gabriel.autenticacao.config;
 
 import com.dev.gabriel.autenticacao.utils.KeyUtils;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.proc.SecurityContext;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
@@ -26,5 +34,12 @@ public class SecurityConfig {
   public void loadKeys() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
     this.privateKey = (RSAPrivateKey) KeyUtils.decodeKey(this.caminhoChavePrivada);
     this.publicKey = (RSAPublicKey) KeyUtils.decodeKey(this.caminhoChavePublica);
+  }
+
+  @Bean
+  public JwtEncoder jwtEncoder() {
+    JWK jwk = new RSAKey.Builder(this.publicKey).privateKey(this.privateKey).build();
+    ImmutableJWKSet<SecurityContext> jwkSet = new ImmutableJWKSet<>(new JWKSet(jwk));
+    return new NimbusJwtEncoder(jwkSet);
   }
 }
