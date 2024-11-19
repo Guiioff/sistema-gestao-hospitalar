89078bdfed94bd2e5ada47062ebdf2e1f7d65805
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-19 20:02:45 -0300

    Criando handler para as exceções do sistema

---
+++ backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/exeption/CustomExceptionHandler.java
@@ -0,0 +1,6 @@
+package com.dev.gabriel.autenticacao.exeption;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {}
