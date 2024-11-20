1196c1553d3efdc3b5516e2bd79ec7087af39e57
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-20 13:28:21 -0300

    Implementando um handler para as exceções da aplicação

---
+++ backend/predicao-doenca-do-coracao/src/main/java/com/dev/gabriel/predicao_doenca_do_coracao/exception/handler/CustomExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.dev.gabriel.predicao_doenca_do_coracao.exception.handler;
+
+import com.dev.gabriel.predicao_doenca_do_coracao.dto.response.ErroResponse;
+import com.dev.gabriel.predicao_doenca_do_coracao.exception.exceptions.GenericaException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+import java.util.List;
+
+@RestControllerAdvice
+@Slf4j
+public class CustomExceptionHandler {
+  @ExceptionHandler(GenericaException.class)
+  public ResponseEntity<ErroResponse> handleGenericaException(GenericaException ex) {
+    ErroResponse response =
+        ErroResponse.builder()
+            .status(ex.getStatus().value())
+            .erro(ex.getErro())
+            .detalhe(List.of(ex.getDetalhe()))
+            .timestamp(Instant.now())
+            .build();
+
+    log.error(ex.getErro() + ". Detalhes: {}", response.detalhe());
+    return ResponseEntity.status(ex.getStatus()).body(response);
+  }
+}
