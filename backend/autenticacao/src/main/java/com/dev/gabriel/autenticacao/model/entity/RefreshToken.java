989de18bfa0d0c42e93ca5184c24fd145102e23d
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-20 00:07:50 -0300

    Adicionando as anotações JPA à classe RefreshToken

--- backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/model/entity/RefreshToken.java
+++ backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/model/entity/RefreshToken.java
@@ -1,11 +1,24 @@
 package com.dev.gabriel.autenticacao.model.entity;
 
+import jakarta.persistence.*;
+
 import java.time.Instant;
 import java.util.UUID;
 
+@Entity
+@Table(name = "tb_refresh_token")
 public class RefreshToken {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
+  @Column(nullable = false, unique = true)
   private String token;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "usuario_id", nullable = false)
   private Usuario usuario;
+
+  @Column(nullable = false)
   private Instant expiracao;
 }
