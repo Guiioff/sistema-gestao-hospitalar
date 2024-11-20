12da705fd4d1e176225806179b011ffa66ebc0ab
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-20 10:58:34 -0300

    Modificando a configuração do RabbitMQ para o recurso de dead letter queue

--- backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/config/RabbitMQConfig.java
+++ backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/config/RabbitMQConfig.java
@@ -1,6 +1,6 @@
 package com.dev.gabriel.autenticacao.config;
 
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -11,9 +11,38 @@ public class RabbitMQConfig {
   @Value("${rabbitmq.queue.novo-usuario}")
   private String novoUsuarioNomeQueue;
 
+  @Value("${rabbitmq.queue.novo-usuario-dlq}")
+  private String novoUsuarioNomeDLQ;
+
+  @Value("${rabbitmq.exchange.novo-usuario}")
+  private String nomeExchange;
+
   @Bean
   public Queue novoUsuarioQueue() {
-    return new Queue(novoUsuarioNomeQueue, true);
+    return QueueBuilder.durable(novoUsuarioNomeQueue)
+        .withArgument("x-dead-letter-exchange", nomeExchange)
+        .withArgument("x-dead-letter-routing-key", novoUsuarioNomeDLQ)
+        .build();
+  }
+
+  @Bean
+  public Queue deadLetterQueue() {
+    return new Queue(novoUsuarioNomeDLQ, true);
+  }
+
+  @Bean
+  public DirectExchange directExchange() {
+    return new DirectExchange(nomeExchange);
+  }
+
+  @Bean
+  public Binding novoUsuarioBinding() {
+    return BindingBuilder.bind(novoUsuarioQueue()).to(directExchange()).with(novoUsuarioNomeQueue);
+  }
+
+  @Bean
+  public Binding dlqBinding() {
+    return BindingBuilder.bind(deadLetterQueue()).to(directExchange()).with(novoUsuarioNomeDLQ);
   }
 
   @Bean
