2e5a0b69c622513e338deaa5c93616ca2a5fc3c6
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-19 22:22:05 -0300

    Adicionando as anotações Validation ao dto de login

--- backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/dto/request/LoginRequest.java
+++ backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/dto/request/LoginRequest.java
@@ -1,3 +1,10 @@
 package com.dev.gabriel.autenticacao.dto.request;
 
-public record LoginRequest(String email, String senha) {}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+    @NotBlank(message = "O email fornecido não pode ser nulo ou estar em branco")
+        @Email(message = "O email fornecido tem um formato inválido")
+        String email,
+    @NotBlank(message = "A senha fornecida não pode ser nula ou estar em branco") String senha) {}
