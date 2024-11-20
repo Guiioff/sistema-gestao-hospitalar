f0aa5daaa9483f90e2181e9bdbfebb30ca04a95e
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-20 13:38:04 -0300

    Implementando handler para as exceções da Validation

--- backend/predicao-doenca-do-coracao/src/main/java/com/dev/gabriel/predicao_doenca_do_coracao/exception/handler/CustomExceptionHandler.java
+++ backend/predicao-doenca-do-coracao/src/main/java/com/dev/gabriel/predicao_doenca_do_coracao/exception/handler/CustomExceptionHandler.java
@@ -3,7 +3,10 @@ package com.dev.gabriel.predicao_doenca_do_coracao.exception.handler;
 import com.dev.gabriel.predicao_doenca_do_coracao.dto.response.ErroResponse;
 import com.dev.gabriel.predicao_doenca_do_coracao.exception.exceptions.GenericaException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,6 +16,26 @@ import java.util.List;
 @RestControllerAdvice
 @Slf4j
 public class CustomExceptionHandler {
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErroResponse> handleValidationException(
+      MethodArgumentNotValidException ex) {
+    List<String> mensagensErro =
+        ex.getBindingResult().getAllErrors().stream()
+            .map(DefaultMessageSourceResolvable::getDefaultMessage)
+            .toList();
+
+    ErroResponse response =
+        ErroResponse.builder()
+            .status(HttpStatus.BAD_REQUEST.value())
+            .erro("Dados de entrada inválidos")
+            .detalhe(mensagensErro)
+            .timestamp(Instant.now())
+            .build();
+
+    log.error("Dados de entrada inválidos. Detalhes: {}", response.detalhe());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+  }
+
   @ExceptionHandler(GenericaException.class)
   public ResponseEntity<ErroResponse> handleGenericaException(GenericaException ex) {
     ErroResponse response =
