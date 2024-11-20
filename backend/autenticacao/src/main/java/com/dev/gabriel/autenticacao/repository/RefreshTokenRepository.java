560b2381e7ea3a7210348435fba0b06ffb8dd5ef
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-20 10:10:54 -0300

    Declarando query method para buscar um refresh token pelo seu token

--- backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/repository/RefreshTokenRepository.java
+++ backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/repository/RefreshTokenRepository.java
@@ -4,8 +4,11 @@ import com.dev.gabriel.autenticacao.model.entity.RefreshToken;
 import com.dev.gabriel.autenticacao.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
   void deleteByUsuario(Usuario usuario);
+
+  Optional<RefreshToken> findByToken(String token);
 }
