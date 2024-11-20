bf7e4db72e91c6abe59916458677e1abd867ef91
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-20 00:18:04 -0300

    Implementando método de geração do refresh token

--- backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/service/RefreshTokenService.java
+++ backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/service/RefreshTokenService.java
@@ -1,9 +1,15 @@
 package com.dev.gabriel.autenticacao.service;
 
+import com.dev.gabriel.autenticacao.model.entity.RefreshToken;
+import com.dev.gabriel.autenticacao.model.entity.Usuario;
 import com.dev.gabriel.autenticacao.repository.RefreshTokenRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -12,4 +18,19 @@ public class RefreshTokenService {
 
   @Value("${security.refresh-token-duracao-segundos}")
   private Long refreshTokenDuracaoSegundos;
+
+  @Transactional
+  public String gerarRefreshToken(Usuario usuario) {
+    this.refreshTokenRepository.deleteByUsuario(usuario);
+
+    RefreshToken refreshToken =
+        RefreshToken.builder()
+            .token(UUID.randomUUID().toString())
+            .usuario(usuario)
+            .expiracao(Instant.now().plusSeconds(refreshTokenDuracaoSegundos))
+            .build();
+
+    this.refreshTokenRepository.save(refreshToken);
+    return refreshToken.getToken();
+  }
 }
