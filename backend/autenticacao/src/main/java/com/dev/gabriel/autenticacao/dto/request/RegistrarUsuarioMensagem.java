0243adfd1368259c0b3690272713182a2217b87c
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-19 22:22:20 -0300

    Adicionando as anotações Validation ao dto de mensagem de registro de usuário

--- backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/dto/request/RegistrarUsuarioMensagem.java
+++ backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/dto/request/RegistrarUsuarioMensagem.java
@@ -1,3 +1,12 @@
 package com.dev.gabriel.autenticacao.dto.request;
 
-public record RegistrarUsuarioMensagem(String id, String email, String password, String role) {}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record RegistrarUsuarioMensagem(
+    @NotBlank(message = "O ID fornecido não pode ser nulo ou estar em branco") String id,
+    @NotBlank(message = "O email fornecido não pode ser nulo ou estar em branco")
+        @Email(message = "O email fornecido tem um formato inválido")
+        String email,
+    @NotBlank(message = "A senha fornecida não pode ser nula ou estar em branco") String password,
+    @NotBlank(message = "A role fornecida não pode ser nula ou estar em branco") String role) {}
