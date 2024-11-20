dc47f0fe481807618c9702c0880cc375ef525314
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-20 10:01:09 -0300

    Modificando a injeção do object mapper

--- backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/config/CustomAuthenticationEntryPoint.java
+++ backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/config/CustomAuthenticationEntryPoint.java
@@ -4,16 +4,22 @@ import com.dev.gabriel.autenticacao.dto.response.ErroResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.time.Instant;
 import java.util.List;
 
+@Component
+@RequiredArgsConstructor
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+  private final ObjectMapper objectMapper;
+
   @Override
   public void commence(
       HttpServletRequest request,
@@ -32,7 +38,7 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
             .timestamp(Instant.now())
             .build();
 
-    String responseBody = new ObjectMapper().writeValueAsString(erroResponse);
+    String responseBody = objectMapper.writeValueAsString(erroResponse);
     response.getWriter().write(responseBody);
   }
 }
