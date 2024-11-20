19694cb170a3ca25ec4f1c0ef082064566b6f2b0
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-20 13:12:52 -0300

    Configuração inicial da API

---
+++ backend/predicao-doenca-do-coracao/src/main/java/com/dev/gabriel/predicao_doenca_do_coracao/PredicaoDoencaDoCoracaoApplication.java
@@ -0,0 +1,12 @@
+package com.dev.gabriel.predicao_doenca_do_coracao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredicaoDoencaDoCoracaoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PredicaoDoencaDoCoracaoApplication.class, args);
+  }
+}
---
+++ backend/predicao-doenca-do-coracao/src/test/java/com/dev/gabriel/predicao_doenca_do_coracao/PredicaoDoencaDoCoracaoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.dev.gabriel.predicao_doenca_do_coracao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredicaoDoencaDoCoracaoApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
