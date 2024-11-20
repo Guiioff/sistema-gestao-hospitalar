60562f4c174ccdca17bc9a40ec45779b9c6c5302
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-20 13:42:53 -0300

    Implementando endpoint para as requisições de predição

--- backend/predicao-doenca-do-coracao/src/main/java/com/dev/gabriel/predicao_doenca_do_coracao/controller/PredicaoController.java
+++ backend/predicao-doenca-do-coracao/src/main/java/com/dev/gabriel/predicao_doenca_do_coracao/controller/PredicaoController.java
@@ -1,7 +1,14 @@
 package com.dev.gabriel.predicao_doenca_do_coracao.controller;
 
+import com.dev.gabriel.predicao_doenca_do_coracao.dto.request.PredicaoRequest;
+import com.dev.gabriel.predicao_doenca_do_coracao.dto.response.PredicaoResponse;
 import com.dev.gabriel.predicao_doenca_do_coracao.service.PredicaoService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class PredicaoController {
   private final PredicaoService predicaoService;
+
+  @PostMapping("/predict")
+  public ResponseEntity<PredicaoResponse> predict(@RequestBody @Valid PredicaoRequest request) {
+    PredicaoResponse response = new PredicaoResponse(this.predicaoService.predict(request));
+    return ResponseEntity.status(HttpStatus.OK).body(response);
+  }
 }
