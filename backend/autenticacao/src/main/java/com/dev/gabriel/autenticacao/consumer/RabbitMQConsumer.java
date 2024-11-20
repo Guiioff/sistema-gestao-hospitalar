146440cbcf0aaca677b74f97a8245b1ac803fa8c
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-19 23:12:59 -0300

    Modificando o tratamento de exceções no consumer

--- backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/consumer/RabbitMQConsumer.java
+++ backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/consumer/RabbitMQConsumer.java
@@ -3,6 +3,7 @@ package com.dev.gabriel.autenticacao.consumer;
 import com.dev.gabriel.autenticacao.dto.request.RegistrarUsuarioMensagem;
 import com.dev.gabriel.autenticacao.service.UsuarioService;
 import com.rabbitmq.client.Channel;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -20,20 +21,19 @@ public class RabbitMQConsumer {
 
   @RabbitListener(queues = "${rabbitmq_novo_usuario_queue}")
   public void registrarUsuarioListener(
-      RegistrarUsuarioMensagem mensagem,
+      @Valid RegistrarUsuarioMensagem mensagem,
       Channel channel,
       @Header(AmqpHeaders.DELIVERY_TAG) long deliveryTag) {
     try {
       this.usuarioService.registrarNovoUsuario(mensagem);
       channel.basicAck(deliveryTag, false);
     } catch (Exception e) {
-      log.error(
-          "Erro no processamento da mensagem para cadastro de novo usuário: {}", e.getMessage());
+      log.error("Erro no processamento da mensagem: {}", e.getMessage());
 
       try {
         channel.basicReject(deliveryTag, false);
       } catch (IOException ioException) {
-        log.error("Erro ao processar a rejeição da mensagem: {}", e.getMessage());
+        log.error("Erro ao rejeitar a mensagem: {}", e.getMessage());
       }
     }
   }
