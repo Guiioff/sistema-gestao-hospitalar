8d2404e5dc78693f3ea0db207b344aaf2755528f
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-19 23:38:23 -0300

    Adicionando restrição de comprimento da senha

--- backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/dto/request/RegistrarUsuarioMensagem.java
+++ backend/autenticacao/src/main/java/com/dev/gabriel/autenticacao/dto/request/RegistrarUsuarioMensagem.java
@@ -2,11 +2,14 @@ package com.dev.gabriel.autenticacao.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record RegistrarUsuarioMensagem(
     @NotBlank(message = "O ID fornecido não pode ser nulo ou estar em branco") String id,
     @NotBlank(message = "O email fornecido não pode ser nulo ou estar em branco")
         @Email(message = "O email fornecido tem um formato inválido")
         String email,
-    @NotBlank(message = "A senha fornecida não pode ser nula ou estar em branco") String senha,
+    @NotBlank(message = "A senha fornecida não pode ser nula ou estar em branco")
+        @Size(min = 6, message = "A senha precisa ter, no mínimo, 6 caracteres")
+        String senha,
     @NotBlank(message = "A role fornecida não pode ser nula ou estar em branco") String role) {}
