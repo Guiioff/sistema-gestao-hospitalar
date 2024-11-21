e5d5bef70c750af52de5564aa32ba2ea56c021c0
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-21 00:02:17 -0300

    Implementando método para extrair o token JWT da requisição

--- backend/api-gateway/src/main/java/com/dev/gabriel/api_gateway/security/filter/JwtFiltro.java
+++ backend/api-gateway/src/main/java/com/dev/gabriel/api_gateway/security/filter/JwtFiltro.java
@@ -3,6 +3,7 @@ package com.dev.gabriel.api_gateway.security.filter;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
 
 @Component
 public class JwtFiltro extends AbstractGatewayFilterFactory<JwtFiltro.Config> {
@@ -14,6 +15,15 @@ public class JwtFiltro extends AbstractGatewayFilterFactory<JwtFiltro.Config> {
   @Override
   public GatewayFilter apply(Config config) {
     return null;
+  }
+
+  private String extrairToken(ServerWebExchange exchange) {
+    String cabecalhoAuth = exchange.getRequest().getHeaders().getFirst("Authorization");
+    if (cabecalhoAuth != null && cabecalhoAuth.startsWith("Bearer ")) {
+      return cabecalhoAuth.substring(7);
+    } else {
+      throw new RuntimeException("Token de autenticação inválido ou inexistente");
+    }
   }
 
   public static class Config {}
