6511cead3f86e6edf64c39a4ab27c7563bb41abe
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-21 00:28:23 -0300

    Adicionando o Bean para o decoder do token

--- backend/api-gateway/src/main/java/com/dev/gabriel/api_gateway/config/SecurityConfig.java
+++ backend/api-gateway/src/main/java/com/dev/gabriel/api_gateway/config/SecurityConfig.java
@@ -3,28 +3,30 @@ package com.dev.gabriel.api_gateway.config;
 import com.dev.gabriel.api_gateway.utils.KeyUtils;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
-import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 
 @Configuration
 public class SecurityConfig {
-  @Value("${security.caminho-chave-privada}")
-  private String caminhoChavePrivada;
-
   @Value("${security.caminho-chave-publica}")
   private String caminhoChavePublica;
 
-  private RSAPrivateKey privateKey;
   private RSAPublicKey publicKey;
 
   @PostConstruct
   public void loadKeys() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
-    this.privateKey = (RSAPrivateKey) KeyUtils.decodeKey(this.caminhoChavePrivada);
     this.publicKey = (RSAPublicKey) KeyUtils.decodeKey(this.caminhoChavePublica);
+  }
+
+  @Bean
+  public JwtDecoder jwtDecoder() {
+    return NimbusJwtDecoder.withPublicKey(this.publicKey).build();
   }
 }
