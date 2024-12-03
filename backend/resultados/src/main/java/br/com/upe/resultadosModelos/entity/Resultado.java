a47c17aec5b3b547644298693b6eab91f036c0b0
Emílio <emiliogabriel000@gmail.com>
2024-12-03 14:11:21 -0300

    Criação da entidade de resultado

--- backend/resultados/src/main/java/br/com/upe/resultadosModelos/entity/Resultado.java
+++ backend/resultados/src/main/java/br/com/upe/resultadosModelos/entity/Resultado.java
@@ -1,5 +1,35 @@
 package br.com.upe.resultadosModelos.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Resultado {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long exameId;
+
+    @Column(nullable = false)
+    private String resultado;
+
+    @Column(nullable = false)
+    private Long PacienteId;
+    
+    @Column(nullable = false)
+    private String tipoExame;
+
+ 
 }
