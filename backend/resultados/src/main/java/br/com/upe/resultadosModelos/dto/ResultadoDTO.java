f876929f2ff1b9b634dee8c974c67c0809e739cb
Emílio <emiliogabriel000@gmail.com>
2024-12-03 14:10:57 -0300

    Criação do DTO de resultado

--- backend/resultados/src/main/java/br/com/upe/resultadosModelos/dto/ResultadoDTO.java
+++ backend/resultados/src/main/java/br/com/upe/resultadosModelos/dto/ResultadoDTO.java
@@ -1,5 +1,20 @@
 package br.com.upe.resultadosModelos.dto;
 
-public class ResultadoDTO {
+import com.fasterxml.jackson.annotation.JsonAlias;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResultadoDTO {
+    private Long id;
+    private Long exameId;
+
+    @JsonAlias({"predict", "prediction"})
+    private String resultado;
+
+    private Long pacienteId;
+    private String tipoExame;
 }
