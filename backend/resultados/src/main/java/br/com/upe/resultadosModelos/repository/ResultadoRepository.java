eb8314a485dbc31b54ec20fe3cdf8fa16f04e79e
Emílio <emiliogabriel000@gmail.com>
2024-12-03 14:10:25 -0300

    criação do repository do serviço de resultado

--- backend/resultados/src/main/java/br/com/upe/resultadosModelos/repository/ResultadoRepository.java
+++ backend/resultados/src/main/java/br/com/upe/resultadosModelos/repository/ResultadoRepository.java
@@ -1,5 +1,9 @@
 package br.com.upe.resultadosModelos.repository;
 
-public class ResultadoRepository {
+import br.com.upe.resultadosModelos.entity.Resultado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface ResultadoRepository extends JpaRepository<Resultado, Long> {
 }
