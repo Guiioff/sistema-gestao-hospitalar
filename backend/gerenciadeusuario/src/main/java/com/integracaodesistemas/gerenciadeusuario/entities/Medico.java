188f84aabfd375db5bf8130cd0b7baf6ecf0c0d9
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-12-04 20:08:41 -0300

    Adicionando atributos à entidade Médico

--- backend/gerenciadeusuario/src/main/java/com/integracaodesistemas/gerenciadeusuario/entities/Medico.java
+++ backend/gerenciadeusuario/src/main/java/com/integracaodesistemas/gerenciadeusuario/entities/Medico.java
@@ -9,17 +9,20 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class Medico implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String nome;
-    private String crm;
-    private String telefone;
-    private String email;
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  private String nome;
+  private String crm;
+  private String telefone;
+  private String email;
+  private String senha;
 }
