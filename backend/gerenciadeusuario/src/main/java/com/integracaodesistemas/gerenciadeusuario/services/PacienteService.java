3ecffd0b09a988439173493a0d3f2085b8def8b8
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-12-04 20:09:49 -0300

    Modificando o método de cadastro para enviar mensagem AMQP

--- backend/gerenciadeusuario/src/main/java/com/integracaodesistemas/gerenciadeusuario/services/PacienteService.java
+++ backend/gerenciadeusuario/src/main/java/com/integracaodesistemas/gerenciadeusuario/services/PacienteService.java
@@ -1,7 +1,10 @@
 package com.integracaodesistemas.gerenciadeusuario.services;
 
 import com.integracaodesistemas.gerenciadeusuario.entities.Paciente;
+import com.integracaodesistemas.gerenciadeusuario.producer.NovoUsuarioProducer;
+import com.integracaodesistemas.gerenciadeusuario.producer.dto.NovoUsuarioCadastradoMensagem;
 import com.integracaodesistemas.gerenciadeusuario.repositories.PacienteRepository;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,37 +13,53 @@ import java.util.Optional;
 
 @Service
 public class PacienteService {
+  @Autowired private NovoUsuarioProducer novoUsuarioProducer;
 
-    @Autowired
-    private PacienteRepository pacienteRepository;
+  @Autowired private PacienteRepository pacienteRepository;
 
-    public List<Paciente> listarPacientes(){
-        return pacienteRepository.findAll();
-    }
+  public List<Paciente> listarPacientes() {
+    return pacienteRepository.findAll();
+  }
 
-    public Optional<Paciente> buscarPorId(Long id){
-        return pacienteRepository.findById(id);
-    }
+  public Optional<Paciente> buscarPorId(Long id) {
+    return pacienteRepository.findById(id);
+  }
 
-    public Optional<Paciente> buscarPorCpf(String cpf){
-        return pacienteRepository.findByCpf(cpf);
-    }
+  public Optional<Paciente> buscarPorCpf(String cpf) {
+    return pacienteRepository.findByCpf(cpf);
+  }
 
-    public Paciente salvarPaciente(Paciente paciente){
-        return pacienteRepository.save(paciente);
-    }
+  public Paciente salvarPaciente(Paciente paciente) {
+    Paciente pacienteSalvo = this.pacienteRepository.save(paciente);
+    NovoUsuarioCadastradoMensagem mensagem =
+        new NovoUsuarioCadastradoMensagem(
+            pacienteSalvo.getId().toString(),
+            paciente.getEmail(),
+            senhaCriptografada(paciente.getSenha()),
+            "PACIENTE");
+    this.novoUsuarioProducer.enviarMensagemNovoUsuario(mensagem);
+    return pacienteSalvo;
+  }
 
-    public Optional<Paciente> atualizarPaciente(Long id, Paciente pacienteAtualizado) {
-        return pacienteRepository.findById(id).map(pacienteExistente -> {
-            pacienteExistente.setNome(pacienteAtualizado.getNome());
-            pacienteExistente.setCpf(pacienteAtualizado.getCpf());
-            pacienteExistente.setDataNascimento(pacienteAtualizado.getDataNascimento());
-            pacienteExistente.setEndereco(pacienteAtualizado.getEndereco());
-            pacienteExistente.setTelefone(pacienteAtualizado.getTelefone());
-            return pacienteRepository.save(pacienteExistente);
-        });
-    }
-    public void deletarPaciente(Long id){
-        pacienteRepository.deleteById(id);
-    }
- }
+  public Optional<Paciente> atualizarPaciente(Long id, Paciente pacienteAtualizado) {
+    return pacienteRepository
+        .findById(id)
+        .map(
+            pacienteExistente -> {
+              pacienteExistente.setNome(pacienteAtualizado.getNome());
+              pacienteExistente.setCpf(pacienteAtualizado.getCpf());
+              pacienteExistente.setDataNascimento(pacienteAtualizado.getDataNascimento());
+              pacienteExistente.setEndereco(pacienteAtualizado.getEndereco());
+              pacienteExistente.setTelefone(pacienteAtualizado.getTelefone());
+              return pacienteRepository.save(pacienteExistente);
+            });
+  }
+
+  public void deletarPaciente(Long id) {
+    pacienteRepository.deleteById(id);
+  }
+
+  private static String senhaCriptografada(String senhaRaw) {
+    return BCrypt.hashpw(senhaRaw, BCrypt.gensalt());
+  }
+}
