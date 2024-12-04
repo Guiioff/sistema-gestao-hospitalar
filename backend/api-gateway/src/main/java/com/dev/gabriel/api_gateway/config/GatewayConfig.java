2059c8dc12036dfb3489cca71abc9f4ba0427ba6
MateusVasc <mvaraujo2004@gmail.com>
2024-12-04 09:27:15 -0300

    adiciona rota de agendametos

--- backend/api-gateway/src/main/java/com/dev/gabriel/api_gateway/config/GatewayConfig.java
+++ backend/api-gateway/src/main/java/com/dev/gabriel/api_gateway/config/GatewayConfig.java
@@ -6,14 +6,28 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class GatewayConfig {
-  private final JwtFiltro jwtFiltro;
+    private final JwtFiltro jwtFiltro;
 
-  @Bean
-  public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-    return builder.routes().build();
-  }
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route(
+                        "fazer-agendamento-route",
+                        r ->
+                                r.path("/agendamentos")
+                                        .and().method(HttpMethod.POST)
+                                        .filters(
+                                                f ->
+                                                        f.filter(this.jwtFiltro.apply(new JwtFiltro.Config(List.of("MEDICO")))))
+                                        .uri("lb://GESTAO-AGENDAMENTOS"))
+                .build();
+    }
 }
