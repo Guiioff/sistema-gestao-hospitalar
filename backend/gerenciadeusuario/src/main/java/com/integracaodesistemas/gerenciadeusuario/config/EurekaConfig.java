b3e3f64f258c6cac24a4d38096d12e26382941b1
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-12-05 05:07:04 -0300

    Configurando a conexão com o Eureka Server

---
+++ backend/gerenciadeusuario/src/main/java/com/integracaodesistemas/gerenciadeusuario/config/EurekaConfig.java
@@ -0,0 +1,28 @@
+package com.integracaodesistemas.gerenciadeusuario.config;
+
+import com.netflix.appinfo.ApplicationInfoManager;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class EurekaConfig {
+  private final EurekaClient eurekaClient;
+  private final ApplicationInfoManager applicationInfoManager;
+
+  @EventListener(ApplicationReadyEvent.class)
+  public void reconnectToEureka() {
+    try {
+      this.applicationInfoManager.setInstanceStatus(InstanceInfo.InstanceStatus.UP);
+      this.eurekaClient.getApplicationInfoManager().refreshDataCenterInfoIfRequired();
+    } catch (Exception e) {
+      log.error("Reconexão com o Eureka Server falhou: " + e.getMessage());
+    }
+  }
+}
